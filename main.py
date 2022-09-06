b7d97dd20331500ab133846f459af468b4c87293
luezero <a0970937127@gmail.com>
2022-09-06 22:37:49 +0800

    修正macos支援

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+#coding=utf-8
+
 import os
 import numpy as np
 import pytesseract
 import operator
 import cv2
 import re
+import platform
+
 import pyautogui
 import pyperclip
+import keyboard,time
 
+from time import sleep as t
 from mss import mss
 from PIL import Image
 from dotenv import load_dotenv
@@ -14,13 +20,22 @@ load_dotenv()
 
 reply = os.getenv("REPLY_TEXT")
 
-mon = {'left': 0, 'top': 350, 'width': 500, 'height': 600}
+mon = {'left': 0, 'top': 0, 'width': 400, 'height': 500}
 
-keywords = ['招募', '自', '假', '採', '六', '日']
+# Windows 注意事項
+# C:\Program Files\Tesseract-OCR\tessdata\chi_tra.traineddata 把中文模型放入此位置，看你OCR安裝位置
 
-# Tesseract-OCR\tessdata\chi_tra.traineddata 把中文模型放入此位置
+# MacOS 注意事項
+# /usr/local/Cellar/tesseract/3.05.02[版本號]/share/tessdata 把中文模型放入此位置
+# 鍵盤指令無效記得去看隱私權設定
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+print("輸入文字:"+reply)
+print("作業系統:"+platform.system())
+
+if  platform.system()  == "Windows":
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" 
+elif platform.system()  == "Darwin":
+    pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
 
 with mss() as sct:
     while True:
@@ -38,8 +53,6 @@ with mss() as sct:
 
         result = data.split()
 
-        print(keywords)
-
         a = [i for i,x in enumerate(result) if x.find('招募') != -1]
         b = [i for i,x in enumerate(result) if x.find('自') != -1]
         c = [i for i,x in enumerate(result) if x.find('假') != -1]
@@ -50,12 +63,14 @@ with mss() as sct:
 
         if len(a) > 0 and len(b) > 0 and len(c) > 0 and len(d) > 0 and len(e) > 0:
             if len(f) > 0 or len(g) > 0:
-                print('找到關鍵字')
                 pyperclip.copy(reply)
                 pyperclip.paste()
-                pyautogui.hotkey('ctrl', 'c')
-                pyautogui.hotkey('ctrl', 'v')
-                pyautogui.press('enter')
+                                
+                pyautogui.hotkey('command', 'c', interval=0.25)
+                pyautogui.hotkey('command', 'v', interval=0.25)
+                pyautogui.press('enter', interval=0.25)
+
+                print('找到關鍵字')
                 break
             else:
                 print('找不到關鍵字')
